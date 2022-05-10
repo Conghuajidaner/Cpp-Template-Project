a111bbe51fb0e7d94a723888a984668409e987b9
yaokun Zhai <zhaiyaokun@lixiang.com>
2022-05-10 23:12:32 +0800

    feat(): create base class and design

--- src/Template.cpp
+++ src/Template.cpp
@@ -1,1 +1,26 @@
 #include "Template.h"
+
+Template::Template() {
+    init_();
+}
+
+Template::~Template() {
+    deinit_();
+}
+
+void Template::init_() {
+    switch_ = false;
+    // run_();
+}
+
+void Template::deinit_() {
+    // northing to do
+}
+
+void Template::run_() {
+    switch_ = true;
+    while (switch_) {
+        // printf("test run\n");
+        WAIT_TIME
+    }
+}
